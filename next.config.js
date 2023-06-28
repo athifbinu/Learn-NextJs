00f24d1dcc5be4e5f0d1f52e7d39742a82c0e43b
Athif Binu <binuathif@gmail.com>
2023-06-28 12:41:45 +0530

    img  config fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images:{
-        domains:["images.pixels.com"]
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "**",
+        },
+      ],
     }
 }
 
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,7 @@
 import Image from "next/image"
 import styles from "./page.module.css"
 import Hero from "public/hero.png"
+import Button from "@/components/Button/Button"
 
 export default function Home() {
   return (
@@ -13,7 +14,7 @@ export default function Home() {
         Turning your Idea into Reality. We bring together the teams from the
         global tech industry.
       </p>
- 
+       <Button url="/Portfolio" text="See Works"/>
     </div>
     <div className={styles.item}>
       <Image src={Hero} alt="" className={styles.img} />
